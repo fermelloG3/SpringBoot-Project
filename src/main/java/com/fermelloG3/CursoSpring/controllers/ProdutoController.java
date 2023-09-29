84142d1ca15fe864ebaf8a71af618cf633225e57
ferdemello <110197887+fermelloG3@users.noreply.github.com>
2023-09-29 01:14:13 -0300

    find by name, method in produtoRepositoy interface

--- src/main/java/com/fermelloG3/CursoSpring/controllers/ProdutoController.java
+++ src/main/java/com/fermelloG3/CursoSpring/controllers/ProdutoController.java
@@ -33,9 +33,15 @@ public class ProdutoController {
     public Optional<Produto> obterProdutoPorId(@PathVariable int id){
         return produtoRepository.findById(id);
     }
+    @GetMapping(path = "/nome/{parteNome}")
+    public Iterable<Produto> obterProdutoPorNome(@PathVariable String parteNome){
+        return produtoRepository.findByNomeIsContainingIgnoreCase(parteNome);
+
+    }
     @GetMapping(path = "/pagina/{numerodepagina}/{qtdepagina}")
     public Iterable<Produto> obterProdutoPorPagina(@PathVariable int numerodepagina,
                                                    @PathVariable int qtdepagina){
+        if (qtdepagina >= 5) qtdepagina = 5;
         Pageable page = PageRequest.of(numerodepagina,qtdepagina);
         return produtoRepository.findAll(page);
     }
--- src/main/java/com/fermelloG3/CursoSpring/model/repositories/ProdutoRepository.java
+++ src/main/java/com/fermelloG3/CursoSpring/model/repositories/ProdutoRepository.java
@@ -4,4 +4,6 @@ import com.fermelloG3.CursoSpring.model.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
+
+    public Iterable<Produto> findByNomeIsContainingIgnoreCase(String parteNome);
 }
